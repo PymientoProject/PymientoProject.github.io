6edb9d0bfbee2d376665aad6ec0ae09effc7d78f
Iván Gonzalo Moyano Pérez <ivan.gonzalo.moyano@gmail.com>
2016-09-21 11:53:53 +0200

    Schrödinger's Python stuffs
    intro del proyecto y el directorio Open-the-Box que muestra al 50% si
    hay código de Python o no

---
+++ works/schrodingers-python/open-the-box/holaMundo.py
@@ -0,0 +1,12 @@
+#########################################################
+#
+# The Pymiento Project
+# Project: Schrödinger's Python
+# About: http://www.pymiento.com/works/schrodingers-python
+# Description: Does this file exist into the MicroSD card or not? Of course yes, if you are reading this description is because it exist.
+# Autor: Iván Gonzalo Moyano Pérez
+# Version: 1.0
+#
+#########################################################
+
+print '¡Hola Mundo!'
